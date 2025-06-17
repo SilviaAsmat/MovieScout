40e0eda574b08bf51b466d2096860d82ae24abc1
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-17 14:17:58 -0500

    Commented out class, not in use anymore due to new compose serializable object parameter

--- android_app/app/src/main/java/com/example/movienight20/ui/Destinations.kt
+++ android_app/app/src/main/java/com/example/movienight20/ui/Destinations.kt
@@ -1,15 +1,19 @@
-package com.example.movienight20.ui
+/*
+* Not in use anymore due to compose<>
+* Can be implemented for future use if
+* more screens will be routed/serialized
+* */
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed interface Destinations {
-    val route: String
-}
-
-data object MovieList : Destinations {
-    override val route = "MovieList"
-}
-data object MovieDetails : Destinations {
-    override val route = "MovieDetails"
-}
+// package com.example.movienight20.ui
+//
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.ui.graphics.vector.ImageVector
+//
+//sealed interface Destinations {
+//}
+//
+//data object MovieList : Destinations {
+//}
+//data object MovieDetails : Destinations {
+//}
