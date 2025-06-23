f9e6ba48aae76ad37f66c7467ea6b8d10b93f73a
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-23 10:46:16 -0500

    Removed unused imports

--- android_app/app/src/main/java/com/example/movienight20/MainActivity.kt
+++ android_app/app/src/main/java/com/example/movienight20/MainActivity.kt
@@ -5,16 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.movienight20.ui.MainNavHost
 import com.example.movienight20.ui.theme.MovieDetailsViewModel
