ddd616a5f997011f1e24d20148517fa88218e884
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-26 16:35:49 -0500

    Created view state and view model for the main screen, reusing popularMoviesInfo domain class to create the view state

--- android_app/app/src/main/java/com/example/movienight20/ui/MainScreenViewModel.kt
+++ android_app/app/src/main/java/com/example/movienight20/ui/MainScreenViewModel.kt
@@ -1,4 +1,49 @@
 package com.example.movienight20.ui
 
-class MainScreenViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.movienight20.domain.MoviesRepository
+import com.example.movienight20.domain.PopularMoviesInfo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainScreenViewModel @Inject constructor(
+    private val movieRepo: MoviesRepository
+): ViewModel (){
+    private val mutableViewState = MutableStateFlow<MainScreenViewState>(MainScreenViewState.NONE)
+    val viewState: StateFlow<MainScreenViewState> = mutableViewState
+
+    init {
+        viewModelScope.launch {
+            val popularMoviesResult = movieRepo.getMovies()
+            val nowPlayingResult = movieRepo.getNowPlaying()
+            val popState = popularMoviesResult.map {
+                PopularMoviesInfo(
+                    id = it.id,
+                    title = it.title,
+                    backdropPath = "http://image.tmdb.org/t/p/" + "w1280" + it.backdropPath,
+                    posterPath = "http://image.tmdb.org/t/p/" + "w1280" + it.posterPath,
+                    releaseDate = it.releaseDate,
+                    rating = it.rating
+                )
+            }
+            val nowPlayingState = nowPlayingResult.map {
+                PopularMoviesInfo(
+                    id = it.id,
+                    title = it.title,
+                    backdropPath = "http://image.tmdb.org/t/p/" + "w1280" + it.backdropPath,
+                    posterPath = "http://image.tmdb.org/t/p/" + "w1280" + it.posterPath,
+                    releaseDate = it.releaseDate,
+                    rating = it.rating
+                )
+            }
+            val viewState = MainScreenViewState(popMoviesInfo = popState, nowPlayingMoviesInfo = nowPlayingState)
+
+            mutableViewState.emit(viewState)
+        }
+    }
 }
--- android_app/app/src/main/java/com/example/movienight20/ui/MainScreenViewState.kt
+++ android_app/app/src/main/java/com/example/movienight20/ui/MainScreenViewState.kt
@@ -1,4 +1,15 @@
 package com.example.movienight20.ui
 
-class MainScreenViewState {
+import com.example.movienight20.domain.PopularMoviesInfo
+
+class MainScreenViewState(
+    val popMoviesInfo: List<PopularMoviesInfo>,
+    val nowPlayingMoviesInfo: List<PopularMoviesInfo>
+) {
+    companion object {
+        val NONE = MainScreenViewState(
+            popMoviesInfo = listOf(),
+            nowPlayingMoviesInfo = listOf()
+        )
+    }
 }
