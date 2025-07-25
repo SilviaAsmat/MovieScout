90c25515ffc3fcad2be48b3676e9a750c5b5117a
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-07-25 13:24:59 -0500

    Removed unused files, added new files for recents screen

---
+++ android_app/app/src/main/java/com/example/movienight20/ui/recently_viewed/RecentlyViewedScreenViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.movienight20.ui.recently_viewed
+
+class RecentlyViewedScreenViewModel {
+}
--- android_app/app/src/main/java/com/example/movienight20/ui/recently_viewed/RecentlyViewedViewState.kt
+++ android_app/app/src/main/java/com/example/movienight20/ui/recently_viewed/RecentlyViewedViewState.kt
@@ -1,11 +1,11 @@
 package com.example.movienight20.ui.recently_viewed
 
 import androidx.compose.runtime.Immutable
-import com.example.movienight20.ui.MovieCardInfoViewState
+import com.example.movienight20.ui.search.ResultItemViewState
 
 @Immutable
 sealed class RecentlyViewedViewState {
-    data class Data(val cards: List<MovieCardInfoViewState>): RecentlyViewedViewState()
+    data class Data(val cards: List<ResultItemViewState>): RecentlyViewedViewState()
     data object Loading : RecentlyViewedViewState()
     data object Empty : RecentlyViewedViewState()
 }
